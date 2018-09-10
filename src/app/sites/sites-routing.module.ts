c4bd95e697784ae074a1f68ec7d46cf96ed54c23
edFortune <fortune.edmael1@gmail.com>
2018-09-10 16:51:35 -0400

    add routerLink to nav-bar

--- src/app/sites/sites-routing.module.ts
+++ src/app/sites/sites-routing.module.ts
@@ -7,6 +7,15 @@ import { BlogComponent } from './blog/blog.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { MotRecteurComponent } from './rectorat/mot-recteur/mot-recteur.component';
+import { HistoireComponent } from './rectorat/histoire/histoire.component';
+import { CodeEtiqueComponent } from './rectorat/code-etique/code-etique.component';
+import { VisionComponent } from './rectorat/vision/vision.component';
+import { ScienceAdministrativeComponent } from './programmes/preparatoire/science-administrative/science-administrative.component';
+import { SciencePolitiqueComponent } from './programmes/preparatoire/science-politique/science-politique.component';
+import { EtudiantPostGradueComponent } from './admission/requisition/etudiant-post-gradue/etudiant-post-gradue.component';
+import { EtudiantGradueComponent } from './admission/requisition/etudiant-gradue/etudiant-gradue.component';
+import { EtudiantPreparatoireComponent } from './admission/requisition/etudiant-preparatoire/etudiant-preparatoire.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -16,6 +25,20 @@ const routes: Routes = [
     { path: 'blog', component: BlogComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'login', component: LoginComponent },
+    //Rectorat
+    { path: 'rectorat/recteur', component: MotRecteurComponent },
+    { path: 'rectorat/histoire', component: HistoireComponent },
+    { path: 'rectorat/code-etique', component: CodeEtiqueComponent },
+    { path: 'rectorat/vision', component: VisionComponent },
+    //Programme offert - preparatoire
+    { path: 'programmes/preparatoire/science-administrative', component: ScienceAdministrativeComponent },
+    { path: 'programmes/preparatoire/science-politique', component: SciencePolitiqueComponent },
+    //Admission - Requisition
+    { path: 'admission/etudiant-post-gradue', component: EtudiantPostGradueComponent },
+    { path: 'admission/etudiant-gradue', component: EtudiantGradueComponent },
+    { path: 'admission/etudiant-preparatoire', component: EtudiantPreparatoireComponent },
+
+
 ];
 
 @NgModule({
