cea481ab462bfd3f235e076aad3cb97cbece4441
edFortune <fortune.edmael1@gmail.com>
2018-09-10 11:13:48 -0400

    add more field to candidat form

--- src/app/dashboard/candidat/candidat-new/candidat-new.component.ts
+++ src/app/dashboard/candidat/candidat-new/candidat-new.component.ts
@@ -22,7 +22,15 @@ export class CandidatNewComponent implements OnInit {
     this.candidatInputs = [
       { label: "Nom", props: 'nom', type: 'text' },
       { label: "Nom d'usage", props: 'nomUsage', type: 'text' },
-      { label: "Prénom", props: 'prenom', type: 'text' }
+      { label: "Prénom", props: 'prenom', type: 'text' },
+      { label: "CIN/NIF", props: 'carteId', type: 'text' },
+      { label: "Dernier diplome obtenu", props: 'diplome', type: 'text' },
+      { label: "Date de naissance", props: 'dateNaissance', type: 'dateTime' },
+      { label: "Commune", props: 'commune', type: 'text' },
+      { label: "Adresse", props: 'adresse', type: 'text' },
+      { label: "No téléphone fixe", props: 'telephoneFixe', type: 'text' },
+      { label: "No Portable", props: 'portable', type: 'text' },
+      { label: "E-mail", props: 'email', type: 'text' },
     ];
 
   }
