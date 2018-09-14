b849a2f87327d42daf8e1ce4e049838960e2de82
edFortune <fortune.edmael1@gmail.com>
2018-09-13 22:24:53 -0400

    add student component

--- src/app/dashboard/etudiant/etudiant-routing.module.ts
+++ src/app/dashboard/etudiant/etudiant-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EtudiantListComponent } from './etudiant-list/etudiant-list.component';
+import { EtudiantNewComponent } from './etudiant-new/etudiant-new.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: EtudiantListComponent
+  },
+  {
+    path: 'new', component: EtudiantNewComponent
+  },
+  {
+    path: ':id', component: EtudiantNewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
